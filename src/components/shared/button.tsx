2a626a24fd3e3cc91ab75f98fed215a6f0963b31
Sruul <dashnyam@Saruuls-MacBook-Pro.local>
2026-01-23 23:44:34 +0800

    first button

--- src/components/shared/button.tsx
+++ src/components/shared/button.tsx
@@ -12,7 +12,7 @@ const Button: React.FC<ButtonProps> = ({ text, className = "", ...props }) => {
       {...props}
       className={`group/button overflow-hidden relative px-5 py-3.5 rounded-full flex items-center bg-red-400 justify-center ${className}`}
     >
-      <span className="transition-all duration-400 ease-in-out group-hover/button:-translate-y-[150%] group-hover/button:opacity-0">
+      <span className="transition-all duration-400 ease-in-out group-hover/button:-translate-y-[150%] group-hover/button:opacity-0 ">
         {text}
       </span>
       <span className="absolute top-full left-0 right-0 transition-all duration-400 ease-in-out opacity-0 group-hover/button:-translate-y-full group-hover/button:opacity-100 h-full flex items-center justify-center">
