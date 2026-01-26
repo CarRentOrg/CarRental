4ebd3b996b71e80a09f990fb9388a903cc02b765
Sruul <dashnyam@Saruuls-MacBook-Pro.local>
2026-01-26 20:06:18 +0800

    done

--- src/components/_sections/FeaturedSection.tsx
+++ src/components/_sections/FeaturedSection.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+
 import Title from "../shared/title";
 import { FEATURED_CARS } from "@/constants";
 import CarCard from "../cars/CarCard";
--- src/components/shared/button.tsx
+++ src/components/shared/button.tsx
@@ -26,12 +26,12 @@ const Button: React.FC<ButtonProps> = ({
     <button
       {...props}
       onClick={handleClick}
-      className={`group/button overflow-hidden relative px-5 py-3.5 rounded-full flex items-center bg-white justify-center ${className} text-black`}
+      className={`group/button overflow-hidden relative px-5 py-3.5 rounded-full flex items-center bg-white justify-center ${className} text-black hover:bg-neutral-200 hover:shadow-lg hover:shadow-white/10 hover:scale-99 cursor-pointer transition-all duration-400 ease-in-out `}
     >
-      <span className="transition-all duration-400 ease-in-out group-hover/button:-translate-y-[150%] group-hover/button:opacity-0 ">
+      <span className="transition-all duration-300 ease-in-out group-hover/button:-translate-y-[150%] group-hover/button:opacity-0 ">
         {text}
       </span>
-      <span className="absolute top-full left-0 right-0 transition-all duration-400 ease-in-out opacity-0 group-hover/button:-translate-y-full group-hover/button:opacity-100 h-full flex items-center justify-center">
+      <span className="absolute top-full left-0 right-0 transition-all duration-300 ease-in-out opacity-0 group-hover/button:-translate-y-full group-hover/button:opacity-100 h-full flex items-center justify-center">
         {text}
       </span>
     </button>
